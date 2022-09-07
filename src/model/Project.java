d3e07e6f8383abe7140df7a3445e64fe964ff0d0
Wagner Araujo <wagneraraujo15@gmail.com>
2022-09-07 17:20:05 -0300

    create connection mysql

---
+++ src/model/Project.java
@@ -0,0 +1,69 @@
+package model;
+import java.util.Date;
+public class Project {
+    private int id;
+    private String name;
+    private String description;
+    private Date createAt;
+    private Date updateAt;
+
+    public Project(int id, String name, String description, Date createAt, Date updateAt) {
+
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", createAt=" + createAt +
+                ", updateAt=" + updateAt +
+                '}';
+    }
+}
---
+++ src/util/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package util;
+
+public class ConnectionFactory {
+    public static final String DRIVER = "com.mysql.jdbc.DRIVER";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+}
