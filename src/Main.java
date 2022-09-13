76188aea3c4304f44653203dde5a18d69c805874
Wagner Araujo <wagneraraujo15@gmail.com>
2022-09-12 21:30:10 -0300

    class connection db

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import util.ConnectionFactory;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args){
+
+        Connection c = ConnectionFactory.getConnection();
+
+        //close ex
+        //ConnectionFactory.closeConnection(c);
 
     }
 }
--- src/util/ConnectionFactory.java
+++ src/util/ConnectionFactory.java
@@ -1,8 +1,32 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionFactory {
     public static final String DRIVER = "com.mysql.jdbc.DRIVER";
+    //    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
     public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
     public static final String USER = "root";
     public static final String PASS = "";
+
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error connection DB.");
+        }
+    }
+
+    public static void closeConnection(Connection connection){
+        try{
+            if(connection != null){
+                connection.close();
+            }
+        }catch (Exception ex){
+            throw new RuntimeException("Error close DB");
+        }
+    }
 }
